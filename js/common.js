572fbd4cc28ea89d20bb612dceba0a610889daf2
erpdest02 <erpdest02@gmail.com>
2023-12-18 21:50:27 +0800

    e

--- js/common.js
+++ js/common.js
@@ -4,14 +4,14 @@ function parseUrlSearchParams() {
 
 function decodeBase64(content) {
   if (content) {
-    let str = '';
+    let str = ''
     try {
       str = content.replaceAll(' ', '+').replaceAll('-', '+').replaceAll('_', '/')
     } catch (error) {
       str = content.replace(/ /g, '+').replace(/-/g, '+').replace(/_/g, '/')
     }
     return decodeURIComponent(atob(str).split('').map(function (c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
       }).join('')
     )
   } else {
